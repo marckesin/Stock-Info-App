1e8173a9a223afde1cec7c35b42907d62448dcad
xaveco <192.168.2.1@protonmail.com>
2021-07-17 17:56:18 -0300

    alteração do layout da pagina home

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render("contact", { contact: "Contact" });
+    res.render("contact", { contact: "Contato" });
 });
 
 module.exports = router;
